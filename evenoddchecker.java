b8e830505d1e058f617f9437d41393b688173e49
8095128299 <ingalagianand030@gmail.com>
2025-04-15 11:06:15 +0530

    saved

---
+++ evenoddchecker.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.util.Scanner;
+
+public class even_odd {
+  
+    public static void main(String[] args)
+    {
+        
+        Scanner sc=new Scanner(System.in);
+       System.out.println("Enter a number");
+        int n= sc.nextInt();
+        System.out.println("number is: " + n);
+       if ((n | 1) > n) {
+   
+            System.out.println("Number is Even");
+        }
+        else {
+            System.out.println("Number is Odd");
+        }
+    }
+}
